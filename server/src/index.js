952715ce88e45f3f2addb5248326773762034453
Bindu1724 <madishettibindu1706@gmail.com>
2026-03-30 17:12:44 +0530

    last commit

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const empRoutes = require('./routes/employee.routes');
 
 const app = express();
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://startling-cuchufli-78f449.netlify.app/', credentials: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 
